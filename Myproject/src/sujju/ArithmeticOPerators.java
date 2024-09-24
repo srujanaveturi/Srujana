7bc92215df5d13fa526d7f3a7d1d6f2a58ed6b25
Sujith Veturi <veturisujith1999@gmail.com>
2024-09-24 19:54:50 +0530

    Seventh commit

---
+++ Myproject/src/sujju/ArithmeticOPerators.java
@@ -0,0 +1,27 @@
+package sujju;
+
+public class ArithmeticOPerators {
+
+	public static void main(String[] args) {
+		
+		int a=20, b=15;
+		
+		int sum =a+b;
+		System.out.println("addition :" + sum);
+		
+		int diff =a-b;
+		System.out.println("sub :" + diff);
+		
+		int prod =a*b;
+		System.out.println("mul :" +prod);
+		
+		int quotient =a/b;
+		System.out.println("div :" +quotient);
+		 
+		int remainder =a%b;
+		System.out.println("remainder :" +remainder);
+		
+
+	}
+
+}
