070510a1414fdce0cb90798fc356c1385dc8df6d
Sujith Veturi <veturisujith1999@gmail.com>
2024-09-24 19:39:18 +0530

    Sixth commit

---
+++ Myproject/src/sujju/IfelseIfladder.java
@@ -0,0 +1,34 @@
+package sujju;
+
+public class IfelseIfladder {
+
+	public static void main(String[] args) {
+		
+        int score =63;
+		
+		if (score>=90)
+		{
+			System.out.println("Grade A");
+		}
+		else if (score>=80)
+		{
+			System.out.println("Grade B");
+		}
+		else if(score>=70)
+		{
+			System.out.println("Grade c");
+		}
+		else if(score>=60)
+		{
+			System.out.println("Grade d");
+		}
+		else
+		{
+			System.out.println("Grade F (Fail)");
+		}
+	}
+
+
+	}
+
+
