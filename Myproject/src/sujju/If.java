e03124e56506d2bba636f05301d0db67b76baa6c
Sujith Veturi <veturisujith1999@gmail.com>
2024-09-24 09:54:12 +0530

    Fifth commit

---
+++ Myproject/src/sujju/If.java
@@ -0,0 +1,17 @@
+package sujju;
+
+public class If {
+
+	public static void main(String[] args) {
+		int age=11;
+		if (age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			System.out.println("you are not eligible");
+		}
+	}
+}
+
