c7890b64113471726e740e86fc7da37d3ae8668c
Sujith Veturi <veturisujith1999@gmail.com>
2024-09-27 14:06:26 +0530

    twelvecommit

---
+++ Myproject/src/sujju/Teranaryoperators.java
@@ -0,0 +1,13 @@
+package sujju;
+
+public class Teranaryoperators {
+
+	public static void main(String[] args) {
+		int a=5,b=3;
+		
+		 // Ternary operation
+        int max = (a > b) ? a : b;
+        System.out.println("Max value is: " + max);
+	}
+
+}
