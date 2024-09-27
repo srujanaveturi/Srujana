2b1e897794a8d8429c089163ff11c57286f46454
Srujana Veturi <veturisrujana1010@gmail.com>
2024-09-27 13:55:02 +0530

    Eleven commit

---
+++ Myproject/src/sujju/Logicaloperators.java
@@ -0,0 +1,26 @@
+package sujju;
+
+public class Logicaloperators {
+
+	public static void main(String[] args) {
+		int a=15, b=7, c=18;
+		
+		//logicl And
+		System.out.println("(a>b)&&(b<c) :" +((a>b)&&(b<c)));
+		
+		//logical Or
+		System.out.println("(a>b)||(b<c) :" +((a>b)||(b<c)));
+		
+		//logical Not
+		System.out.println("!(a>b) :" +!(a>b));
+		
+	}
+}	
+		
+		
+		
+		
+		
+	
+
+
