9a41af87ed5f48a2912c42b2aae64bba464a0a89
Srujana Veturi <veturisrujana1010@gmail.com>
2024-09-27 13:28:04 +0530

    Eight commit

---
+++ Myproject/src/sujju/AssigmentOperators.java
@@ -0,0 +1,39 @@
+package sujju;
+
+public class AssigmentOperators {
+
+	public static void main(String[] args) {
+		int a=15;
+		
+		//add and assign
+		a+=5;
+		System.out.println("a after +=" +a);
+		
+		a-=4;
+		System.out.println("a after -=" +a);
+		
+		a*=6;
+		System.out.println("a after *=" +a);
+		
+		a/=2;
+		System.out.println("a after /=" +a);
+		
+		a%=3;
+		System.out.println("a after %=" +a);
+	}
+
+}
+	
+		
+		
+				
+		
+
+		
+		
+		
+		
+
+	
+
+
