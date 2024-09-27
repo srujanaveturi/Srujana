5989e78cf3bf5294dd7c59cc27781b6ff66c165c
Srujana Veturi <veturisrujana1010@gmail.com>
2024-09-27 13:38:34 +0530

    Ninthcommit

---
+++ Myproject/src/sujju/Realtionaloperators.java
@@ -0,0 +1,17 @@
+package sujju;
+
+public class Realtionaloperators {
+
+	public static void main(String[] args) {
+          int a=15,b=12;
+          
+          System.out.println("a==b :" +(a==b));
+          System.out.println("a!=b :" +(a!=b));
+          System.out.println("a>b :" +(a>b));
+          System.out.println("a<b :" +(a<b));
+          System.out.println("a>=b :" +(a>=b));
+          System.out.println("a<=b :" +(a<=b));
+          
+	}
+
+}
