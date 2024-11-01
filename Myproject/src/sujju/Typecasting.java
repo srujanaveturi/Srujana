c50355d67669f8c1c4f4b3646c36f9ab27c0872a
Sujith Veturi <veturisujith1999@gmail.com>
2024-11-01 12:45:58 +0530

    fourteen commit

---
+++ Myproject/src/sujju/Typecasting.java
@@ -0,0 +1,27 @@
+package sujju;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		
+		byte b=120; 
+		int  i=b;     
+		System.out.println(i);
+		
+		float f=15.14f;
+		double d=f;
+		System.out.println(d);
+		
+		 
+		
+		double f1=10.52d;
+		long l= (long)f1;
+		System.out.println(l);
+		
+		float f2=34.56f;
+		int li=(int) f2;
+		System.out.println(li);
+
+	}
+
+}
